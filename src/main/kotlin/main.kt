6d94d2da87dcda34c96577e053887d14473f2613
jBosak <jbosak98@gmail.com>
2020-03-10 21:12:14 +0100

    dowafpefaf

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,17 @@
+fun main(){
+    println("xd")
+//    generate()
+}
+
+fun generate(n:Int,w:Int,s:Int,output_file:String) = generatePopulation(n,w,s,output_file)
+
+
+
+fun generatePopulation(objectsSize:Int,maxWeight:Int,maxKnapsackSize:Int,output_file:String) {
+
+}
+
+
+fda
+
+data class Item(val weight:Int, val size:Int)
