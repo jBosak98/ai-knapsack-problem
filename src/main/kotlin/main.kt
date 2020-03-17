404f7d2b469edcf544f7b8d7db35d2aa5e733132
jBosak <jbosak98@gmail.com>
2020-03-17 22:17:01 +0100

    generatePopulation and gitignore

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,18 +1,23 @@
-fun main(){
+import kotlin.random.Random
+
+fun main() {
     println("xd")
-//    generate()
+    generate(100,100,100,"xd")
 }
 
-fun generate(n:Int,w:Int,s:Int,output_file:String) = generatePopulation(n,w,s,output_file)
+fun generate(n: Int, w: Int, s: Int, output_file: String) = generatePopulation(n, w, s, output_file)
+
+fun generatePopulation(objectsSize: Int, knapsackCapacity: Int, knapsackSize: Int, output_file: String) {
+    val items = arrayOfNulls<Unit>(objectsSize).map {
+        val weight = Random.nextInt(1,10 * knapsackCapacity / objectsSize)
+        val size = Random.nextInt(1, 10*knapsackSize / objectsSize)
+        val price = Random.nextInt(1,objectsSize)
+        return@map Item(weight,size,price)
+    }
+    print(items)
 
 
-
-fun generatePopulation(objectsSize:Int,maxWeight:Int,maxKnapsackSize:Int,output_file:String) {
 
 }
 
-
-fda
-
-data class Item(val weight:Int, val size:Int)
-fsdfdsfsfd
+data class Item(val weight: Int, val size: Int, val price:Int)
