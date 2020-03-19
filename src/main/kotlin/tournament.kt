03e9beed00f08ad2295230f3cb1be3e7da8c543b
jBosak <jbosak98@gmail.com>
2020-03-19 23:51:52 +0100

    population.individual.indices in tournament

--- src/main/kotlin/tournament.kt
+++ src/main/kotlin/tournament.kt
@@ -1,15 +1,14 @@
 import model.Individual
 import model.Population
 
-fun tournament(population: Population, tournamentSize:Int): Population {
-    val individuals = population.individuals.map {
+fun tournament(population: Population, tournamentSize: Int): Population {
+    val individuals = population.individuals.indices.map {
         population.individuals.shuffled().take(tournamentSize).reduce { acc, individual ->
-            when(individual.evaluation > acc.evaluation){
-                true ->individual
-                else -> acc
+            when (individual.evaluation > acc.evaluation) {
+                true -> individual
+                false -> acc
             }
         }
     }
     return Population(individuals)
-
 }
