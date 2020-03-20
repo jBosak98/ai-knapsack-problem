a753e8b91096d4f12cd0d6a3967a1cc22c782b28
jBosak <jbosak98@gmail.com>
2020-03-20 21:38:08 +0100

    values for geneticAlgorithm updated

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,21 +1,21 @@
 import generators.*
 
 
-const val iterations = 1000
-const val populationSize = 1000u
+const val iterations = 5000
+const val populationSize = 100u
 
 const val crossoverProbability = 0.5
-const val mutationProbability = 0.5
+const val mutationProbability = 0.003
 
-const val tournamentSize = 5
+const val tournamentSize = 22
 
 const val taskFilename = "task"
 
 
 @ExperimentalUnsignedTypes
 fun main() {
-    val task = createTaskFromFile("task")
-    val (_, knapsackCapacity, knapsackSize, items) = task
+    val task = createTaskFromFile(taskFilename)
+    val (generatedItemsQuantity, knapsackCapacity, knapsackSize, items) = task
 
     val fulfilment = createKnapsackConstrainsCheck(knapsackSize.toInt(), knapsackCapacity.toInt())
     val creationAlgorithm = getRandomAlgorithm(fulfilment, task)
@@ -25,10 +25,12 @@ fun main() {
 
     val crossover = generateCrossover(crossoverProbability, createIndividual)
     val mutation = generateMutation(mutationProbability, createIndividual, items)
-    val selection = generateTournament(tournamentSize)
+    val selection = generatePopulationTournament(tournamentSize)
 
     val epoch = generateEpoch(crossover, mutation, selection)
     val population = initPopulation(populationSize, creationAlgorithm, createIndividual)
     val finalPopulation = (0..iterations).toList().fold(population, epoch)
+
+
 }
 
