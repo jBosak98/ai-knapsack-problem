de96de314d3c07498d068bbd1dadf12003123cdf
jBosak <jbosak98@gmail.com>
2020-03-24 19:28:00 +0100

    random algorithm fix

--- src/main/kotlin/getRandomAlgorithm.kt
+++ src/main/kotlin/getRandomAlgorithm.kt
@@ -1,25 +1,29 @@
 import model.Item
 import model.Task
+import kotlin.random.Random
 
 
-fun getRandomAlgorithm(fulfilment: (List<Item>) -> Boolean, task: Task): (Int) -> List<Item> {
-
-    fun fillKnapsack(fulfilment: (List<Item>) -> Boolean): (items: List<Item>, item: Item) -> List<Item> {
-        return fun(items: List<Item>, item: Item): List<Item> {
-            val newItems = items + item
-            return when (fulfilment(newItems)) {
-                true -> newItems
-                else -> items
-            }
+private fun fillKnapsack(fulfilment: (List<Item>) -> Boolean): (items: List<Item>, item: Item) -> List<Item> {
+    return fun(items: List<Item>, item: Item): List<Item> {
+        val newItems = items + item
+        return when (fulfilment(newItems)) {
+            true -> newItems
+            else -> items
         }
     }
+}
+fun getRandomAlgorithm(fulfilment: (List<Item>) -> Boolean, task: Task): (Int) -> List<Item> {
 
     return fun(_: Int): List<Item> {
+        val itemsQuantity = Random.nextInt(0, task.items.size)
         return task
             .items
-            .toList()
             .shuffled()
-            .fold(emptyList(), fillKnapsack(fulfilment))
+            .take(itemsQuantity)
+            .fold(emptyList(), fillKnapsack(
+//                fulfilment)
+                fun(_:List<Item>) = true)
+            )
     }
 
 }
